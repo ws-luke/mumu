a9c588726d2f3ea49fd25f91a83d4b5d50f5b706
ABOOS <hirosi840101@gmail.com>
2024-10-31 22:27:41 +0800

    將header 改用元件

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import HeaderView from '@/components/HeaderView.vue'
 </script>
 
 <template>
+  <HeaderView></HeaderView>
   <RouterView />
 </template>
 
