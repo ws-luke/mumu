fa99273769c77f3802e6bcedb9c59cebfbc7a78c
hirosi <hirosi840101@gmail.com>
2024-11-07 14:11:41 +0800

    Updates

---
+++ assets/CartView-DTTapcjw.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as r}from"./index-vJx_Uke7.js";const t={};function o(n,a){return r(),c("div")}const _=e(t,[["render",o]]);export{_ as default};
---
+++ assets/CustomersView-DgWt6r2K.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./index-vJx_Uke7.js";const r={};function t(s,n){return o(),c("div")}const a=e(r,[["render",t]]);export{a as default};
