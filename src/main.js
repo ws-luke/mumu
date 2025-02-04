0357e85a027ac35539c87734d33b9eb67581b183
hirosi <hirosi840101@gmail.com>
2025-02-04 20:18:33 +0800

    新增pwa功能

--- src/main.js
+++ src/main.js
@@ -3,13 +3,23 @@ import 'bootstrap'
 import '@/assets/all.scss'
 import './assets/main.css'
 import 'vue3-toastify/dist/index.css';
+import { registerSW } from 'virtual:pwa-register'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import Vue3Toastify from 'vue3-toastify';
 import App from './App.vue'
 import router from './router'
-
+const updateSW = registerSW({
+    onNeedRefresh() {
+      if (confirm('有新版本可用，是否立即更新？')) {
+        updateSW(true)
+      }
+    },
+    onOfflineReady() {
+      console.log('PWA 已準備好，可以離線使用')
+    }
+  })
 const app = createApp(App)
 
 app.use(createPinia())
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,40 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'MUMU',
+        short_name: 'PWA',
+        description: '3C配件批發網站',
+        theme_color: '#e3e8db',
+        background_color: '#455c35',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/app-icon/196.png',
+            sizes: '196x196',
+            type: 'image/png'
+          },
+          {
+            src: '/p512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,jpg,svg}']
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
