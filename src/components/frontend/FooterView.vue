8e28756d4fd5c71d08e0ea6a8ad2aa5c6f63a877
ABOOS <hirosi840101@gmail.com>
2025-02-21 00:02:00 +0800

    修改footer

--- src/components/frontend/FooterView.vue
+++ src/components/frontend/FooterView.vue
@@ -1,10 +1,6 @@
 <template>
   <footer>
-<<<<<<< HEAD
     <div class="container text-center">© 2025 MUMU 鼎沐科技股份有限公司</div>
-=======
-    <div class="container text-center">© 2025 MUMU 3C配件批發網</div>
->>>>>>> 067c0e37e2c35acf68143ac3397b85f4e94fd6bf
   </footer>
 </template>
 <style scoped>
